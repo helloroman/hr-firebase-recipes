320d381ae998cf890a05e9a658458ae1ee4dbfd3
Adam Romanski <helloroman.vlog@gmail.com>
2023-01-07 10:41:57 +0100

    Storage start commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Root from './routes/Root.jsx'
+import {initializeApp} from 'firebase/app';
 import {
+  createBrowserRouter, createRoutesFromElements,
   RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route
-} from "react-router-dom";
-import {initializeApp} from "firebase/app";
-import {getAuth, GoogleAuthProvider} from 'firebase/auth';
-import {Root} from "./routes/Root.jsx";
+  Route,
+} from 'react-router-dom';
 import {AuthProvider} from "./providers/AuthProvider";
 import {ProtectedRoute} from "./helpers/ProtectedRoute";
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
 import {Posts} from "./routes/Posts";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBbM3sRo8o08wsNzOEVuR9TOgJjfJecxsI",
+  authDomain: "roman-auth-recipe.firebaseapp.com",
+  projectId: "roman-auth-recipe",
+  storageBucket: "roman-auth-recipe.appspot.com",
+  messagingSenderId: "905047776324",
+  appId: "1:905047776324:web:9120a1cff7e4e391e5b011"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const provider = new GoogleAuthProvider();
 export const auth = getAuth(app);
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Root/>}>
-    <Route index element={<p>Hello React Router!</p>} />
-    <Route path="posts" element={<ProtectedRoute />}>
-      <Route index element={<Posts />} />
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root/>}>
+      <Route index element={<h1>Hello world</h1>}/>
+      <Route path="posts" element={<ProtectedRoute/>}>
+        <Route index element={<Posts />}/>
+      </Route>
     </Route>
-  </Route>
-));
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router}/>
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import {NavLink, Outlet} from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import {useAuth} from "../providers/AuthProvider.jsx";
+import {NavLink, Outlet} from "react-router-dom";
+import './Root.styles.scss';
 
-export const Root = () => {
+const Root = () => {
   const {user, handleSignIn, handleSignOut} = useAuth();
 
   return (
-    <>
-      <h1>Hello {user ? user.displayName : 'World!'}</h1>
-      <button onClick={handleSignIn}>Sign In</button>
-      <button onClick={handleSignOut}>Sign Out</button>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/posts">Posts</NavLink>
+    <div className="layout">
+      <nav className="layout__navigation">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="posts">Posts</NavLink>
+      </nav>
+      <div className="layout__login">
+        {user ? <Typography>Hello <strong>{user.displayName}!</strong></Typography> : null}
+        <Button variant="contained" onClick={handleSignIn}>Sign in</Button>
+        <Button variant="contained" onClick={handleSignOut}>Sign out</Button>
+      </div>
       <Outlet />
-    </>
+    </div>
   )
-};
+}
+
+export default Root
